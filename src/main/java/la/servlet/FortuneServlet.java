feba38902349d5b28ddf75ee333d33944bdc4678
villageworks <villageworks@gmail.com>
2023-07-19 23:58:56 +0900

    課題JM30 Step-4

--- src/main/java/la/servlet/FortuneServlet.java
+++ src/main/java/la/servlet/FortuneServlet.java
@@ -1,6 +1,7 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.util.Random;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,15 +18,24 @@ import la.bean.FortuneBean;
 @WebServlet("/FortuneServlet")
 public class FortuneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private String[] colors = new String[] {"赤", "黃", "白"};
+	private String[] items = new String[] {"タオル", "カバン", "腕時計"};
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int month = 9;
-		String item = "タオル";
-		String color = "白";
-		int rank = 6;
+		// リクエストパラメータを取得
+		int month = Integer.parseInt(request.getParameter("month"));
+		// 乱数を発生させる準備
+		Random random = new Random();
+		// ラッキーアイテムの取得
+		String item = items[random.nextInt(3)];
+		// ラッキーカラーの取得
+		String color = colors[random.nextInt(3)];
+		// ランクの取得
+		int rank = random.nextInt(12) + 1;
 		// FortuneBeanをインスタンス化
 		FortuneBean bean = new FortuneBean(month, item, color, rank);
 		// リクエストスコープに登録
