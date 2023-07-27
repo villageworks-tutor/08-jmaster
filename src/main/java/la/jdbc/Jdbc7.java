5dab932de227b652bb1b00f6ed00c6338f77f998
villageworks <villageworks@gmail.com>
2023-07-27 18:03:33 +0900

    追加課題JM41 Step-3

--- src/main/java/la/jdbc/Jdbc7.java
+++ src/main/java/la/jdbc/Jdbc7.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Jdbc7 {
 
@@ -16,45 +15,34 @@ public class Jdbc7 {
 	private static final String DB_URL = "jdbc:postgresql:sample";
 	private static final String DB_USER = "student";
 	private static final String DB_PASSWORD = "himitu";
-	
+
 	public static void main(String[] args) {
-		
+
 		// 実行するSQLの設定
-		String sql = "SELECT * FROM emp ORDER BY age DESC LIMIT ?";
-		
-		try (Scanner scanner = new Scanner(System.in)) {
-			// ユーザからの入力値を種痘
-			System.out.print("上位の人数を入力してください：");
-			int upperLimit = scanner.nextInt();
-			
+		String sql = "SELECT min(age), max(age), avg(age) FROM emp";
+
+		try {
 			// JDBCドライバのロード
 			Class.forName(DB_DRIVER);
 			try (// データベース接続オブジェクトの取得
-				 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-				 // SQL実行オブジェクトの取得
-				 PreparedStatement pstmt = con.prepareStatement(sql);
-				) {
-				// プレースホルダにデータをバインド
-				pstmt.setInt(1, upperLimit);
-			try (// SQLの実行と結果セットの取得
-					 ResultSet rs = pstmt.executeQuery();
-					) {
-					// 結果セットの表示
-					while(rs.next()) {
-						System.out.print(rs.getInt("code"));
-						System.out.print("\t");
-						System.out.print(rs.getString("name"));
-						System.out.print("\t");
-						System.out.print(rs.getString("age"));
-						System.out.print("\t");
-						System.out.print(rs.getString("tel"));
-						System.out.println();
-					}
-				}	
+					Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+					// SQL実行オブジェクトの取得
+					PreparedStatement pstmt = con.prepareStatement(sql);
+					// SQLの実行と結果セットの取得
+					ResultSet rs = pstmt.executeQuery();) {
+				// 結果セットの表示
+				while (rs.next()) {
+					System.out.println("年齢の最小は：" + rs.getInt(1) + "歳");
+					System.out.println("年齢の最大は：" + rs.getInt(2) + "歳");
+					System.out.println("年齢の平均は：" + rs.getDouble(3) + "歳");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			
-		} catch (ClassNotFoundException | SQLException e) {
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
+
 	}
 }
