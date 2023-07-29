ecad2cc4ce490411a0fdfb20fb9869a6eea320ee
villageworks <villageworks@gmail.com>
2023-07-30 04:23:33 +0900

    ItemDAOのメソッドの記述順を「検索系→登録系→更新系→削除系」の順に並べ直した

--- src/main/java/kadai5/dao/ItemDAO.java
+++ src/main/java/kadai5/dao/ItemDAO.java
@@ -74,61 +74,38 @@ public class ItemDAO {
 	}
 
 	/**
-	 * 価格で並べ換える
-	 * @param orderByAsc 昇順の場合はtrue、それ以外はfalse
-	 * @return List<ItemBean> 並べ替えられた商品リスト
+	 * 指定した商品番号の商品を取得する
+	 * @param code 取得対象の商品の商品番号
+	 * @return ItemBean | null 商品番号に対応した商品がある場合はItemBeanのいインスタンス、それ以外はnull
 	 * @throws DAOException
 	 */
-	public List<ItemBean> sortByPrice(boolean orderByAsc) throws DAOException {
-		// 実行するSQLを設定
-		String sql = "SELECT * FROM item ORDER BY price";
-		// 並べ替えが降順である場合：SQLの並べ替えモードを追加
-		if (!orderByAsc) sql += " DESC";
+	public ItemBean findByPrimaryKey(int code) throws DAOException {
+		// 実行するSQLの設定
+		String sql = "SELECT * FROM item WHERE code = ?";
 		try (// データベースに接続
 			 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			 // SQL実行オブジェクトの取得
+			 // SQL実行オブジェクトを取得
 			 PreparedStatement pstmt = con.prepareStatement(sql);
-			 // SQLの実行と結果セットの取得
-			 ResultSet rs = pstmt.executeQuery();
 			) {
-			//　結果セットから商品リストへの詰替え
-			List<ItemBean> list = new ArrayList<ItemBean>();
-			while (rs.next()) {
-				int code = rs.getInt("code");
-				String name = rs.getString("name");
-				int price = rs.getInt("price");
-				ItemBean bean = new ItemBean(code, name, price);
-				list.add(bean);
+			// プレースホルダにデータをバインド
+			pstmt.setInt(1, code);
+			try (// SQLの実行と結果セットの取得
+				 ResultSet rs = pstmt.executeQuery();) {
+				// 結果セットからインスタンスを生成
+				ItemBean bean = null;
+				if (rs.next()) {
+					String name =  rs.getString("name");
+					int price = rs.getInt("price");
+					bean = new ItemBean(code, name, price);
+				}
+				return bean;
 			}
-			return list;
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DAOException("レコードの取得に失敗しました。");
 		}
-	}
-
-	/**
-	 * 商品を追加する
-	 * @param item 追加する商品
-	 * @throws DAOException
-	 */
-	public void add(ItemBean item) throws DAOException {
-		// 実行するSQLの設定
-		String sql = "INSERT INTO item (name, price) VALUES (?, ?)";
-		try (// データベースに接続
-			 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			 // SQL実行オブジェクトの取得
-			 PreparedStatement pstmt = con.prepareStatement(sql);) {
-			// プレースホルダにデータをバインド
-			pstmt.setString(1, item.getName());
-			pstmt.setInt(2, item.getPrice());
-			// SQLの実行
-			pstmt.executeUpdate();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new DAOException("レコードの追加に失敗しました。");
-		}
+		
 	}
 
 	/**
@@ -166,65 +143,67 @@ public class ItemDAO {
 			e.printStackTrace();
 			throw new DAOException("レコードの取得に失敗しました。");
 		}
+		
 	}
 
 	/**
-	 * 指定された商品番号の商品を削除する
-	 * @param code 削除対象商品の商品番号
+	 * 価格で並べ換える
+	 * @param orderByAsc 昇順の場合はtrue、それ以外はfalse
+	 * @return List<ItemBean> 並べ替えられた商品リスト
 	 * @throws DAOException
 	 */
-	public void delete(int code) throws DAOException {
-		// 実行するSQLの設定
-		String sql = "DELETE FROM item WHERE code = ?";
+	public List<ItemBean> sortByPrice(boolean orderByAsc) throws DAOException {
+		// 実行するSQLを設定
+		String sql = "SELECT * FROM item ORDER BY price";
+		// 並べ替えが降順である場合：SQLの並べ替えモードを追加
+		if (!orderByAsc) sql += " DESC";
 		try (// データベースに接続
 			 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			 // SQL実行オブジェクトを取得
+			 // SQL実行オブジェクトの取得
 			 PreparedStatement pstmt = con.prepareStatement(sql);
-			 ) {
-			// プレースホルダにデータをバインド
-			pstmt.setInt(1, code);
-			// SQLの実行
-			pstmt.executeUpdate();
-			
+			 // SQLの実行と結果セットの取得
+			 ResultSet rs = pstmt.executeQuery();
+			) {
+			//　結果セットから商品リストへの詰替え
+			List<ItemBean> list = new ArrayList<ItemBean>();
+			while (rs.next()) {
+				int code = rs.getInt("code");
+				String name = rs.getString("name");
+				int price = rs.getInt("price");
+				ItemBean bean = new ItemBean(code, name, price);
+				list.add(bean);
+			}
+			return list;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DAOException("レコードの削除に失敗しました。");
+			throw new DAOException("レコードの取得に失敗しました。");
 		}
 		
 	}
 
 	/**
-	 * 指定した商品番号の商品を取得する
-	 * @param code 取得対象の商品の商品番号
-	 * @return ItemBean | null 商品番号に対応した商品がある場合はItemBeanのいインスタンス、それ以外はnull
+	 * 商品を追加する
+	 * @param item 追加する商品
 	 * @throws DAOException
 	 */
-	public ItemBean findByPrimaryKey(int code) throws DAOException {
+	public void add(ItemBean item) throws DAOException {
 		// 実行するSQLの設定
-		String sql = "SELECT * FROM item WHERE code = ?";
+		String sql = "INSERT INTO item (name, price) VALUES (?, ?)";
 		try (// データベースに接続
 			 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			 // SQL実行オブジェクトを取得
-			 PreparedStatement pstmt = con.prepareStatement(sql);
-			) {
+			 // SQL実行オブジェクトの取得
+			 PreparedStatement pstmt = con.prepareStatement(sql);) {
 			// プレースホルダにデータをバインド
-			pstmt.setInt(1, code);
-			try (// SQLの実行と結果セットの取得
-				 ResultSet rs = pstmt.executeQuery();) {
-				// 結果セットからインスタンスを生成
-				ItemBean bean = null;
-				if (rs.next()) {
-					String name =  rs.getString("name");
-					int price = rs.getInt("price");
-					bean = new ItemBean(code, name, price);
-				}
-				return bean;
-			}
+			pstmt.setString(1, item.getName());
+			pstmt.setInt(2, item.getPrice());
+			// SQLの実行
+			pstmt.executeUpdate();
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DAOException("レコードの取得に失敗しました。");
+			throw new DAOException("レコードの追加に失敗しました。");
 		}
+		
 	}
 
 	/**
@@ -251,8 +230,32 @@ public class ItemDAO {
 			e.printStackTrace();
 			throw new DAOException("レコードの更新に失敗しました。");
 		}
+		
 	}
 	
-	
+	/**
+	 * 指定された商品番号の商品を削除する
+	 * @param code 削除対象商品の商品番号
+	 * @throws DAOException
+	 */
+	public void delete(int code) throws DAOException {
+		// 実行するSQLの設定
+		String sql = "DELETE FROM item WHERE code = ?";
+		try (// データベースに接続
+			 Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			 // SQL実行オブジェクトを取得
+			 PreparedStatement pstmt = con.prepareStatement(sql);
+			 ) {
+			// プレースホルダにデータをバインド
+			pstmt.setInt(1, code);
+			// SQLの実行
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの削除に失敗しました。");
+		}
+		
+	}
 	
 }
